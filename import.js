58c972461a8e7f36f3fa407cfa52365a7acadb2b
Charmander <~@charmander.me>
2019-02-28 21:04:23 -0800

    Initial commit
    It’s not really done, but I’m just going to use it.

---
+++ import.js
@@ -0,0 +1,312 @@
+'use strict';
+
+const assert = require('assert').strict;
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const {inspect} = require('util');
+
+const TOKEN_ENCODE_START = 'a'.charCodeAt(0);
+const KEY_ALPHABET = 'abcdefghijkmnopqrstuwxyz13456789';  // like z-base-32
+const KEY_LENGTH = 24;  // 120 bits
+const ROUND_TO_NEAREST = 256 * 1024;  // 256 KiB
+const ID_BYTES = 8;
+const SORT_LOCALE = 'en';
+const SORT_OPTIONS = {
+	sensitivity: 'base',
+	numeric: true,
+};
+
+const KEY_PATH = path.join(__dirname, 'secret-key');
+const STORAGE_ROOT = path.join(__dirname, 'storage');
+const TEMPORARY_ROOT = path.join(__dirname, 'temp');
+const INDEX_PATH = path.join(STORAGE_ROOT, 'index');
+
+const chomp = text =>
+	text.endsWith('\n') ?
+		text.endsWith('\r\n') ?
+			text.slice(0, -2) :
+			text.slice(0, -1) :
+		text;
+
+const tokenEncode = bytes => {
+	const result = Buffer.alloc(2 * bytes.length);
+	let i = 0;
+
+	for (const byte of bytes) {
+		result[i++] = (byte >>> 4) + TOKEN_ENCODE_START;
+		result[i++] = (byte & 0xf) + TOKEN_ENCODE_START;
+	}
+
+	return result.toString('ascii');
+};
+
+const generateKey = () => {
+	const bytes = crypto.randomBytes(KEY_LENGTH);
+	let result = '';
+
+	for (let i = 0; i < bytes.length; i++) {
+		result += KEY_ALPHABET.charAt(bytes[i] & 31);
+	}
+
+	return result;
+};
+
+const EMPTY = new Uint8Array();
+
+class DerivableKey {
+	constructor(id) {
+		assert(Buffer.isBuffer(id) && id.length === 16);
+		this._id = id;
+	}
+
+	derive(key) {
+		const cipher = crypto.createCipheriv('AES-128-ECB', key, EMPTY);
+		cipher.setAutoPadding(false);
+		const derived = cipher.update(this._id);
+		assert.equal(cipher.final().length, 0);
+		return derived;
+	}
+}
+
+const ENCRYPTION_KEY = new DerivableKey(Buffer.from('ed29594ac9e17e910c344f7a12f8979a', 'hex'));
+const HMAC_KEY       = new DerivableKey(Buffer.from('5c2cbd044fbad7e79d61a91bb5a47108', 'hex'));
+const ID_KEY         = new DerivableKey(Buffer.from('8eb0866b9d601c0c335a47ca6019a183', 'hex'));
+const INDEX_SECRET   = new DerivableKey(Buffer.from('d19e547ec98c7d9ed9a80e8da01824e8', 'hex'));
+
+const readKeySetSync = () => {
+	let fd = null;
+	let keyText;
+
+	try {
+		fd = fs.openSync(KEY_PATH, 'wx', 0o600);
+	} catch (error) {
+		if (error.code !== 'EEXIST') {
+			throw error;
+		}
+	}
+
+	if (fd === null) {
+		keyText = chomp(fs.readFileSync(KEY_PATH, 'ascii'));
+
+		if (keyText.length !== KEY_LENGTH || ![...keyText].every(c => KEY_ALPHABET.includes(c))) {
+			throw new Error('Invalid key in ' + KEY_PATH);
+		}
+	} else {
+		keyText = generateKey();
+		console.error('Generated a new key:', keyText);
+		fs.writeFileSync(fd, keyText, 'ascii');
+		fs.closeSync(fd);
+	}
+
+	const keyBytes =
+		crypto.createHash('sha256')
+			.update(keyText)
+			.digest().slice(0, 16);
+
+	return {
+		encryptionKey: ENCRYPTION_KEY.derive(keyBytes),
+		hmacKey: HMAC_KEY.derive(keyBytes),
+		idKey: ID_KEY.derive(keyBytes),
+		indexSecret: INDEX_SECRET.derive(keyBytes),
+	};
+};
+
+const getHmac = ({hmacKey}, iv, encrypted) =>
+	crypto.createHmac('sha384', hmacKey)
+		.update(iv)
+		.update(encrypted)
+		.digest();
+
+const getId = ({idKey}, fileBytes) =>
+	crypto.createHmac('sha512', idKey)
+		.update(fileBytes)
+		.digest().slice(0, ID_BYTES);
+
+const getEncrypted = (keySet, fileBytes) => {
+	const paddedSize = fileBytes.length + (ROUND_TO_NEAREST - (fileBytes.length % ROUND_TO_NEAREST)) % ROUND_TO_NEAREST;
+	const encodedBytes = Buffer.alloc(4 + paddedSize);
+	encodedBytes.writeUInt32LE(fileBytes.length, 0);
+	fileBytes.copy(encodedBytes, 4);
+
+	const iv = Buffer.alloc(16);
+	crypto.randomFillSync(iv, 0, 12);  // high 12 bytes are nonce (2^32 blocks > 4 GB)
+
+	const cipher = crypto.createCipheriv('AES-128-CTR', keySet.encryptionKey, iv);
+	const encrypted = cipher.update(encodedBytes);
+	assert.equal(cipher.final().length, 0);
+
+	return {
+		encrypted,
+		iv,
+		mac: getHmac(keySet, iv, encrypted),
+		id: getId(keySet, fileBytes),
+	};
+};
+
+const getDecrypted = (keySet, combinedBytes) => {
+	assert(combinedBytes.length >= 16);
+
+	const mac = combinedBytes.slice(0, 48);
+	const iv = combinedBytes.slice(48, 64);
+	const encrypted = combinedBytes.slice(64);
+
+	const expectedMac = getHmac(keySet, iv, encrypted);
+
+	if (!crypto.timingSafeEqual(mac, expectedMac)) {
+		throw new Error('Invalid MAC');
+	}
+
+	const decipher = crypto.createDecipheriv('AES-128-CTR', keySet.encryptionKey, iv);
+	const encodedBytes = decipher.update(encrypted);
+	assert.equal(decipher.final().length, 0);
+
+	const length = encodedBytes.readUInt32LE(0);
+
+	if (length > encodedBytes.length - 4) {
+		throw new Error('Invalid length');
+	}
+
+	return encodedBytes.slice(4, 4 + length);
+};
+
+const readIndexSync = (keySet, indexPath) => {
+	const indexBytes = getDecrypted(keySet, fs.readFileSync(indexPath));
+	const index = new Map();
+
+	let offset = 0;
+
+	while (offset < indexBytes.length) {
+		const nameLength = indexBytes.readUInt8(offset);
+		offset++;
+
+		if (offset + nameLength + ID_BYTES > indexBytes.length) {
+			throw new Error('Invalid length');
+		}
+
+		const name = indexBytes.toString('utf8', offset, offset + nameLength);
+		offset += nameLength;
+
+		const id = indexBytes.slice(offset, offset + ID_BYTES);
+		offset += ID_BYTES;
+
+		if (index.has(name)) {
+			throw new Error('Duplicate name in index');
+		}
+
+		index.set(name, id);
+	}
+
+	return index;
+};
+
+const writeIndexSync = (keySet, index, indexPath) => {
+	let indexSize = 0;
+	const nameLengths = [];
+
+	const sortedIndex = Array.from(index).sort(([a], [b]) => a.localeCompare(b, SORT_LOCALE, SORT_OPTIONS));
+
+	for (const [name, id] of sortedIndex) {
+		const nameLength = Buffer.byteLength(name, 'utf8');
+
+		if (nameLength > 255) {
+			throw new Error(`Name ${inspect(name)} too long`);
+		}
+
+		indexSize += 1 + nameLength + ID_BYTES;
+		nameLengths.push(nameLength);
+	}
+
+	const indexBytes = Buffer.alloc(indexSize);
+	let offset = 0;
+	let i = 0;
+
+	for (const [name, id] of sortedIndex) {
+		const nameLength = nameLengths[i++];
+
+		offset = indexBytes.writeUInt8(nameLength, offset);
+		offset += indexBytes.write(name, offset, 'utf8');
+		offset += id.copy(indexBytes, offset);
+	}
+
+	assert.equal(offset, indexSize);
+
+	const {encrypted, iv, mac} = getEncrypted(keySet, indexBytes);
+
+	const temporaryPath = getTemporaryPath();
+
+	const fd = fs.openSync(temporaryPath, 'wx');
+	fs.writeSync(fd, mac);
+	fs.writeSync(fd, iv);
+	fs.writeSync(fd, encrypted);
+	fs.closeSync(fd);
+	fs.renameSync(temporaryPath, indexPath);
+};
+
+const getTemporaryPath = () =>
+	path.join(
+		TEMPORARY_ROOT,
+		crypto.randomBytes(6).toString('base64')
+			.replace(/\+/g, '-')
+			.replace(/\//g, '_')
+	);
+
+const getStoragePath = id =>
+	path.join(
+		STORAGE_ROOT,
+		tokenEncode(id)
+	);
+
+const mergeIndexSync = (keySet, index, newEntries) => {
+	for (const [name, filePath] of newEntries) {
+		const fileBytes = fs.readFileSync(filePath);
+		const {encrypted, iv, mac, id} = getEncrypted(keySet, fileBytes);
+
+		const temporaryPath = getTemporaryPath();
+		const storagePath = getStoragePath(id);
+
+		const fd = fs.openSync(temporaryPath, 'wx');
+		fs.writeSync(fd, mac);
+		fs.writeSync(fd, iv);
+		fs.writeSync(fd, encrypted);
+		fs.closeSync(fd);
+		fs.renameSync(temporaryPath, storagePath);
+
+		const existingId = index.get(name);
+
+		if (existingId !== undefined && !id.equals(existingId)) {
+			throw new Error(`Index already has different entry with name ${inspect(name)}`);
+		}
+
+		index.set(name, id);
+	}
+};
+
+const keySet = readKeySetSync();
+
+let index;
+
+try {
+	index = readIndexSync(keySet, INDEX_PATH);
+} catch (error) {
+	if (error.code !== 'ENOENT') {
+		throw error;
+	}
+
+	index = new Map();
+}
+
+const toMerge = [];
+
+for (let i = 2; i < process.argv.length; i++) {
+	const importPath = process.argv[i];
+	toMerge.push([path.basename(importPath, '.opus'), importPath]);
+}
+
+mergeIndexSync(keySet, index, toMerge);
+
+console.log(`Index contains ${index.size} file(s)`);
+
+writeIndexSync(keySet, index, INDEX_PATH);
+
+fs.writeFileSync(path.join(STORAGE_ROOT, 'index-secret'), tokenEncode(keySet.indexSecret));
---
+++ server.js
@@ -0,0 +1,172 @@
+'use strict';
+
+const crypto = require('crypto');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+
+const STORAGE_ROOT = path.join(__dirname, 'storage');
+const STORAGE_REQUEST = /^\/storage\/(index|[a-p]{16})$/;
+
+const indexSecret = fs.readFileSync(path.join(STORAGE_ROOT, 'index-secret'), 'ascii');
+const expectedAuthorization = Buffer.from('index-secret ' + indexSecret, 'ascii');
+const isExpectedAuthorization = authorization => {
+	if (authorization === undefined) {
+		return false;
+	}
+
+	const authorizationBytes = Buffer.from(authorization, 'ascii');
+
+	return (
+		authorizationBytes.length === expectedAuthorization.length &&
+		crypto.timingSafeEqual(authorizationBytes, expectedAuthorization)
+	);
+};
+
+class StaticResource {
+	constructor(bytes, {contentType, cacheControl}) {
+		this.bytes = bytes;
+		this.contentType = contentType;
+		this.cacheControl = cacheControl;
+		this.etag =
+			'"' +
+			crypto.createHash('sha512')
+				.update(bytes)
+				.digest()
+				.toString('base64', 0, 9) +
+			'"';
+	}
+
+	respond(req, res) {
+		res.setHeader('Cache-Control', this.cacheControl);
+		res.setHeader('ETag', this.etag);
+
+		if (req.headers['if-none-match'] === this.etag) {
+			res.statusCode = 304;
+			res.end();
+			return;
+		}
+
+		res.setHeader('Content-Type', this.contentType);
+		res.end(this.bytes);
+	}
+}
+
+const viewerResource = new StaticResource(
+	fs.readFileSync(path.join(__dirname, 'viewer.html')),
+	{
+		contentType: 'text/html;charset=utf-8',
+		cacheControl: 'public, no-cache, max-age=2592000',
+	}
+);
+
+const indexResource = new StaticResource(
+	fs.readFileSync(path.join(STORAGE_ROOT, 'index')),
+	{
+		contentType: 'application/octet-stream',
+		cacheControl: 'no-cache, max-age=2592000',
+	}
+);
+
+const sendText = (res, text) => {
+	res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+	res.end(text);
+};
+
+const respond = (req, res) => {
+	res.setHeader('Content-Security-Policy', "frame-ancestors 'none'; default-src 'none'; form-action 'none'; script-src 'unsafe-inline'; connect-src 'self'; media-src blob:");
+	res.setHeader('Strict-Transport-Security', 'max-age=31536000');
+
+	if (req.headers.host !== expectedHost) {
+		res.statusCode = 400;
+		sendText(res, 'Unexpected Host header');
+		return;
+	}
+
+	if (req.url === '/') {
+		viewerResource.respond(req, res);
+		return;
+	}
+
+	if (!isExpectedAuthorization(req.headers.authorization)) {
+		res.statusCode = 401;
+		res.setHeader('WWW-Authenticate', 'index-secret');
+		sendText(res, 'Not authorized');
+		return;
+	}
+
+	const match = STORAGE_REQUEST.exec(req.url);
+
+	if (match === null) {
+		res.statusCode = 404;
+		sendText(res, 'Not found');
+		return;
+	}
+
+	if (match[1] === 'index') {
+		indexResource.respond(req, res);
+	} else {
+		const fileStream = fs.createReadStream(path.join(STORAGE_ROOT, match[1]));
+
+		const openError = error => {
+			fileStream.off('error', openError);
+			fileStream.off('open', openSuccess);
+
+			if (error.code === 'ENOENT') {
+				res.statusCode = 404;
+				sendText(res, 'Not found');
+			} else {
+				res.statusCode = 500;
+				sendText(res, 'Server error');
+			}
+
+			console.error('Error serving %s: %O', req.url, error);
+		};
+
+		const openSuccess = () => {
+			fileStream.off('error', openError);
+			fileStream.off('open', openSuccess);
+
+			res.setHeader('Cache-Control', 'max-age=31536000, immutable');
+			res.setHeader('Content-Type', 'application/octet-stream');
+			fileStream.pipe(res);
+
+			fileStream.on('error', error => {
+				fileStream.unpipe(res);
+				res.destroy(error);
+				console.error(error);
+			});
+		};
+
+		fileStream.on('error', openError);
+		fileStream.on('open', openSuccess);
+	}
+};
+
+if (process.argv.length < 4 || process.argv.length > 5) {
+	console.error('Usage: node server.js <host-header> <listen-port> [<listen-host>]');
+	process.exitCode = 1;
+	return;
+}
+
+const expectedHost = process.argv[2];
+const listenPort = process.argv[3];
+const listenHost = process.argv[4];
+
+if (!listenPort.startsWith('/') && listenHost === undefined) {
+	console.error('host required with port');
+	process.exitCode = 1;
+	return;
+}
+
+const server = https.createServer({
+	cert: fs.readFileSync('cert.crt'),
+	key: fs.readFileSync('cert.key'),
+	ciphers: 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384',
+	honorCipherOrder: false,
+	minVersion: 'TLSv1.2',
+}, respond);
+
+server.listen(listenPort, listenHost, process.env.HOST, () => {
+	console.log('listening at', server.address());
+});
